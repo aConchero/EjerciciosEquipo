2c419207f0a87a4912ff467578352c42a13f358b
Alejandro <alejandroconcherogayan@fp.oceanoatlantico.org>
2021-04-14 13:01:23 +0200

    Ejercicio 8

--- EjerciciosEquipo/Ejercicio08.cs
+++ EjerciciosEquipo/Ejercicio08.cs
@@ -17,8 +17,27 @@ namespace EjerciciosEquipo
 
         private void button1_Click(object sender, EventArgs e)
         {
-            int num1 = int.Parse(numero1.Text);
-            int num2 = int.Parse(numero1.Text);
+            int a = int.Parse(numero1.Text);
+            int b = int.Parse(numero2.Text);
+            int temporal = 0;             
+            while (b != 0) {                 
+                temporal = b;                 
+                b = a % b;                 
+                a = temporal;              
+            }             
+            resultado.Text = a + "";
+            /*
+            int a = Math.Max(n1, n2);
+            int b = Math.Min(n1, n2);
+            int resto;
+            do
+            {
+                resto = b;
+                b = a % b;
+                a = resto;
+            } while (b != 0);
+
+            resultado.Text = "" + a;*/
         }
     }
 }
