611e2b6fd1b9c237378a443706ab5fd3bc61b37c
Nils <nilsschindlervelilla@fp.oceanoatlantico.org>
2021-04-14 13:09:42 +0200

    subir

--- EjerciciosEquipo/Ejercicio07.Designer.cs
+++ EjerciciosEquipo/Ejercicio07.Designer.cs
@@ -37,25 +37,27 @@ namespace EjerciciosEquipo
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(29, 80);
+            this.textBox1.Location = new System.Drawing.Point(25, 67);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(125, 27);
+            this.textBox1.Size = new System.Drawing.Size(110, 23);
             this.textBox1.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(29, 43);
+            this.label1.Location = new System.Drawing.Point(25, 32);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(197, 20);
+            this.label1.Size = new System.Drawing.Size(155, 15);
             this.label1.TabIndex = 1;
             this.label1.Text = "Vector separados por comas";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(29, 126);
+            this.button1.Location = new System.Drawing.Point(25, 94);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(94, 29);
+            this.button1.Size = new System.Drawing.Size(82, 22);
             this.button1.TabIndex = 2;
             this.button1.Text = "Multiplicar";
             this.button1.UseVisualStyleBackColor = true;
@@ -63,20 +65,22 @@ namespace EjerciciosEquipo
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(29, 177);
+            this.textBox2.Location = new System.Drawing.Point(25, 133);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(125, 27);
+            this.textBox2.Size = new System.Drawing.Size(110, 23);
             this.textBox2.TabIndex = 3;
             // 
             // Ejercicio07
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(700, 338);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Ejercicio07";
             this.Text = "Ejercicio07";
             this.ResumeLayout(false);
