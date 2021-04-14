b5b0ef8d2e5793e4b6903c49748cf887df306ee5
Alejandro <alejandroconcherogayan@fp.oceanoatlantico.org>
2021-04-14 10:54:26 +0200

    Termino el menu

--- EjerciciosEquipo/Form1.Designer.cs
+++ EjerciciosEquipo/Form1.Designer.cs
@@ -64,6 +64,12 @@ namespace EjerciciosEquipo
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -304,7 +310,7 @@ namespace EjerciciosEquipo
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(95, 80);
+            this.button1.Location = new System.Drawing.Point(140, 70);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(101, 33);
             this.button1.TabIndex = 1;
@@ -314,39 +320,105 @@ namespace EjerciciosEquipo
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(259, 80);
+            this.button2.Location = new System.Drawing.Point(140, 137);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(100, 33);
             this.button2.TabIndex = 2;
-            this.button2.Text = "button2";
+            this.button2.Text = "Ejercicio 2";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(415, 80);
+            this.button3.Location = new System.Drawing.Point(140, 204);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(104, 33);
+            this.button3.Size = new System.Drawing.Size(100, 33);
             this.button3.TabIndex = 3;
-            this.button3.Text = "button3";
+            this.button3.Text = "Ejercicio 3";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(583, 80);
+            this.button4.Location = new System.Drawing.Point(140, 268);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(102, 33);
+            this.button4.Size = new System.Drawing.Size(100, 33);
             this.button4.TabIndex = 4;
-            this.button4.Text = "button4";
+            this.button4.Text = "Ejercicio 4";
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(141, 336);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(100, 33);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Ejercicio 5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(360, 70);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(106, 33);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "Ejercicio 6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(360, 137);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(106, 33);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "Ejercicio 7";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(360, 204);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(106, 33);
+            this.button8.TabIndex = 8;
+            this.button8.Text = "Ejercicio 8";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(360, 268);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(106, 33);
+            this.button9.TabIndex = 9;
+            this.button9.Text = "Ejercicio 9";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(360, 336);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(106, 33);
+            this.button10.TabIndex = 10;
+            this.button10.Text = "Ejercicio 10";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
@@ -398,6 +470,12 @@ namespace EjerciciosEquipo
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
     }
 }
 
