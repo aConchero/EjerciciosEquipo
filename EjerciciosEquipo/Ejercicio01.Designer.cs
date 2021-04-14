43621bca711d11d6edf2d84f0ca1451e5c016de1
Andres <andresgutierrezclimente@fp.oceanoatlantico.org>
2021-04-14 10:56:46 +0200

    Ejercicio 01 --> Completado

--- EjerciciosEquipo/Ejercicio01.Designer.cs
+++ EjerciciosEquipo/Ejercicio01.Designer.cs
@@ -29,12 +29,54 @@ namespace EjerciciosEquipo
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Factorial = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(28, 29);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(150, 31);
+            this.textBox1.TabIndex = 0;
+            // 
+            // Factorial
+            // 
+            this.Factorial.Location = new System.Drawing.Point(201, 29);
+            this.Factorial.Name = "Factorial";
+            this.Factorial.Size = new System.Drawing.Size(112, 34);
+            this.Factorial.TabIndex = 1;
+            this.Factorial.Text = "Factorial";
+            this.Factorial.UseVisualStyleBackColor = true;
+            this.Factorial.Click += new System.EventHandler(this.Factorial_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(348, 29);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(150, 31);
+            this.textBox2.TabIndex = 2;
+            // 
+            // Ejercicio01
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form2";
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.Factorial);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Ejercicio01";
+            this.Text = "Ejercicio01";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button Factorial;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
